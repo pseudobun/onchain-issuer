36a503743979a6e0c8cd81df6724b73908e0c666
Urban Vidovič <urbanfoundit@gmail.com>
2024-03-10 14:35:39 +0100

    chore: setup formatters
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/contracts/src/Contract.sol
+++ packages/contracts/src/Contract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Contract {
+    uint public value;
+
+    function setValue(uint _value) public {
+        value = _value;
+    }
+}
