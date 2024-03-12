5ac71ca68b21cfbc9f1f0805ced932e0c0b819b9
Urban Vidovič <urbanfoundit@gmail.com>
2024-03-12 13:57:16 +0100

    fix: biome configs and lint
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- packages/contracts/test/Issuer.t.ts
+++ packages/contracts/test/Issuer.t.ts
@@ -1,7 +1,9 @@
-const { expect } = require('chai');
+import { expect } from 'chai';
 
 describe('Counter contract', () => {
   it('Should increment the number correctly', async () => {
+    console.log('Hey');
+
     expect(true).to.equal(true);
   });
 });
